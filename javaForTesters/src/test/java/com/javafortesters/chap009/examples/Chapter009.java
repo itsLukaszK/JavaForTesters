c28c0143083c337678fd6d6f538f0ad110e06575
Ł-Komputer\Ł <lukasz.patryk.krawczyk@gmail.com>
2017-09-17 21:14:28 +0200

    Ch 9

--- javaForTesters/src/test/java/com/javafortesters/chap009/examples/Chapter009.java
+++ javaForTesters/src/test/java/com/javafortesters/chap009/examples/Chapter009.java
@@ -3,6 +3,8 @@ package com.javafortesters.chap009.examples;
 import com.javafortesters.domainentities.User;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 public class Chapter009 {
@@ -34,5 +36,42 @@ public class Chapter009 {
         assertEquals("user100", users100[99].getUsername());
         assertEquals("password100", users100[99].getPassword());
     }
+
+    @Test
+    public void sortWorkdaysArrayAndAssertResult() {
+        String[] workdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        Arrays.sort(workdays);
+        assertEquals("Friday", workdays[0]);
+
+        String[] workdaysMixedCase = {"monday", "Tuesday", "Wednesday", "thursday", "Friday"};
+        Arrays.sort(workdaysMixedCase);
+        assertEquals("monday", workdaysMixedCase[3]);
+    }
+
+    public void print2DIntArray(int[][] multi) {
+        for (int[] outer : multi) {
+            if (outer == null) {
+                System.out.print("null");
+            } else {
+                for (int inner : outer) {
+                    System.out.print(inner + ",");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    @Test
+    public void createATriangle() {
+        int[][] multi = new int[16][];
+        for (int i = 0; i < 16; i++) {
+            multi[i] = new int[i + 1];
+            for (int j = 0; j < i + 1; j++) {
+                multi[i][j] = j;
+            }
+        }
+        print2DIntArray(multi);
+    }
+
 
 }
