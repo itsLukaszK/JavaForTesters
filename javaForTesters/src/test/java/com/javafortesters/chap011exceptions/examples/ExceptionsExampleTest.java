2bd9ea36bc22166b4b182e1051565af0d24c70ee
Ł-Komputer\Ł <lukasz.patryk.krawczyk@gmail.com>
2017-09-26 18:53:33 +0200

    Ch 11

--- javaForTesters/src/test/java/com/javafortesters/chap011exceptions/examples/ExceptionsExampleTest.java
+++ javaForTesters/src/test/java/com/javafortesters/chap011exceptions/examples/ExceptionsExampleTest.java
@@ -5,10 +5,23 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ExceptionsExampleTest {
-    @Test
-    public void throwANullPointerException(){
-        Integer age=18;
-        String ageAsString = age.toString();
+    @Test //(expected = NullPointerException.class)
+    public void throwANullPointerException() {
+        Integer age = null;
+        String ageAsString;
+
+        try {
+            ageAsString = age.toString();
+        } catch (NullPointerException e) {
+            System.out.println("getMessage - " + e.getMessage());
+            System.out.println("getStackTrace - " + e.getStackTrace());
+            System.out.println("printStackTrace");
+            e.printStackTrace();
+
+            age = 18;
+            ageAsString = age.toString();
+        }
+
         String yourAge =
                 "You are " + ageAsString + " years old";
         assertEquals("You are 18 years old", yourAge);
