9c65c2bd04433fea32b97146a2a79891cd53d561
Ł-Komputer\Ł <lukasz.patryk.krawczyk@gmail.com>
2017-10-03 20:05:53 +0200

    Ch 14

--- javaForTesters/src/test/java/com/javafortesters/chap014/examples/Chapter014JUnitExplored.java
+++ javaForTesters/src/test/java/com/javafortesters/chap014/examples/Chapter014JUnitExplored.java
@@ -2,11 +2,12 @@ package com.javafortesters.chap014.examples;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 public class Chapter014JUnitExplored {
     @Test
-    public void AllOfTheAsserts() {
+    public void allOfTheAsserts() {
         int number1 = 1;
         assertEquals(1, number1);
         int number2 = 2;
@@ -21,5 +22,28 @@ public class Chapter014JUnitExplored {
         String number3 = null;
         assertNull(number3);
         assertSame(number1, number2minus1);
+    }
+
+    @Test
+    public void assertThatInsteadOfAllOfTheAsserts() {
+        int number1 = 1;
+        assertThat(number1, is(1));
+        int number2 = 2;
+        assertThat(number1, is(not(number2)));
+        assertThat(number1, not(equalTo(number2)));
+        int[] array1 = {1, 2};
+        int[] array2 = {number1, number2};
+        assertThat(array1, equalTo(array2));
+        assertThat(number1, is(not(nullValue())));
+        int number2minus1 = 2 - 1;
+        assertThat(number2, not(equalTo(number1)));
+        String number3 = null;
+        assertThat(number3, is(nullValue()));
+        assertThat(number1, equalTo(number2minus1));
+        assertThat(number3, not(containsString("null")));
+        String number4 = "four";
+        assertThat(number4, containsString("ou"));
+        assertThat(number4, endsWith("our"));
+        assertThat(number4, startsWith("fou"));
     }
 }
