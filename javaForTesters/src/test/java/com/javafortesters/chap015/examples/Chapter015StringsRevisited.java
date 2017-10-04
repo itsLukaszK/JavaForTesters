6aebe0b6b0d0816e63faa0015a42c0e079c655a0
itsLukaszK <lukasz.patryk.krawczyk@gmail.com>
2017-10-04 16:27:48 +0200

    Ch 15

--- javaForTesters/src/test/java/com/javafortesters/chap015/examples/Chapter015StringsRevisited.java
+++ javaForTesters/src/test/java/com/javafortesters/chap015/examples/Chapter015StringsRevisited.java
@@ -2,9 +2,13 @@ package com.javafortesters.chap015.examples;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class Chapter015StringsRevisited {
     @Test
@@ -28,4 +32,28 @@ public class Chapter015StringsRevisited {
         String abcdeFromBytes = new String(bytes);
         assertThat(abcdeFromBytes, is("abcde"));
     }
+
+    @Test
+    public void useRegionMatches() {
+        String hello = "Hello fella";
+        String young = "young lady";
+        assertTrue(hello.regionMatches(9, young, 6, 2));
+    }
+
+    public void findPositionsOfAllOccurrencesInAString(String string, String substring) {
+        List<Integer> integerList = new ArrayList<>();
+        for (int i = 0; i < string.length(); i++) {
+            int substringLocation = string.indexOf(substring, i);
+            if (substringLocation == -1) break;
+            integerList.add(substringLocation);
+            i = substringLocation;
+        }
+        System.out.println(integerList);
+    }
+
+    @Test
+    public void findPositionsOfAllOccurrencesInAHelloString() {
+        findPositionsOfAllOccurrencesInAString("Hello fella", "l");
+    }
+
 }
