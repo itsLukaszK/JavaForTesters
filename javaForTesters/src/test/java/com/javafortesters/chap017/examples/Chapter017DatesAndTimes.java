d66ca40ec9d49eafbbffee92a4b6a0e7ab728931
Ł-Komputer\Ł <lukasz.patryk.krawczyk@gmail.com>
2017-11-05 16:53:59 +0100

    Ch 18

--- javaForTesters/src/test/java/com/javafortesters/chap017/examples/Chapter017DatesAndTimes.java
+++ javaForTesters/src/test/java/com/javafortesters/chap017/examples/Chapter017DatesAndTimes.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.Calendar;
 
+import static java.util.Calendar.JANUARY;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -40,5 +41,45 @@ public class Chapter017DatesAndTimes {
     public void calendarObject() {
         Calendar calendar = Calendar.getInstance();
         System.out.println(calendar.toString());
+    }
+
+    @Test
+    public void useTheOtherCalendarConstants() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2013, Calendar.DECEMBER, 15, 23, 49, 54);
+        assertEquals(calendar.get(Calendar.MONTH), 11);
+        assertEquals(calendar.get(Calendar.YEAR), 2013);
+        assertEquals(calendar.get(Calendar.DAY_OF_MONTH), 15);
+        assertEquals(calendar.get(Calendar.HOUR_OF_DAY), 23);
+        assertEquals(calendar.get(Calendar.MINUTE), 49);
+        assertEquals(calendar.get(Calendar.HOUR), 11);
+        assertEquals(calendar.get(Calendar.AM_PM), Calendar.PM);
+        //Other constants:
+        assertEquals(calendar.get(Calendar.DAY_OF_WEEK), Calendar.SUNDAY);
+        assertEquals(calendar.get(Calendar.WEEK_OF_MONTH), 2);
+        assertEquals(calendar.get(Calendar.DAY_OF_WEEK), 1);
+        assertEquals(calendar.get(Calendar.WEEK_OF_YEAR), 50);
+        assertEquals(calendar.get(Calendar.DAY_OF_YEAR), 349);
+        //Increment and decrement fields:
+        calendar.add(Calendar.DAY_OF_MONTH, -12);
+        calendar.add(Calendar.MONTH, -6);
+        calendar.add(Calendar.YEAR, -2);
+        assertEquals(calendar.get(Calendar.DAY_OF_MONTH), 3);
+        assertEquals(calendar.get(Calendar.MONTH), Calendar.JUNE);
+        assertEquals(calendar.get(Calendar.YEAR), 2011);
+    }
+
+    @Test
+    public void confirmAddMovesTheYear() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2013, Calendar.DECEMBER, 23);
+        calendar.roll(Calendar.DAY_OF_MONTH, 17);
+        assertEquals(calendar.get(Calendar.DAY_OF_MONTH), 9);
+        assertEquals(11, calendar.get(Calendar.MONTH));
+        assertEquals(2013, calendar.get(Calendar.YEAR));
+        calendar.roll(Calendar.DAY_OF_MONTH, -17);
+        calendar.add(Calendar.DAY_OF_MONTH, 17);
+        assertEquals(JANUARY, calendar.get(Calendar.MONTH));
+        assertEquals(2014, calendar.get(Calendar.YEAR));
     }
 }
---
+++ javaForTesters/src/test/java/com/javafortesters/chap018/examples/Chapter018PropertiesAndPropertyFiles.java
@@ -0,0 +1,56 @@
+package com.javafortesters.chap018.examples;
+
+import org.junit.Test;
+
+import java.io.*;
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Chapter018PropertiesAndPropertyFiles {
+    @Test
+    public void createAndListAPropertiesObject() {
+        Properties properties = new Properties();
+        properties.setProperty("name", "bob");
+        properties.setProperty("gender", "male");
+        properties.setProperty("password", "paSSw0rd");
+        assertEquals(3, properties.size());
+        for (String key :
+                properties.stringPropertyNames()) {
+            System.out.println("Key: " + key + ", Value: " + properties.getProperty(key));
+        }
+        properties.list(System.out);
+        assertTrue(properties.containsKey("gender"));
+        assertEquals("bob", properties.getProperty("name"));
+        assertEquals("default", properties.getProperty("permission", "default"));
+    }
+
+    @Test
+    public void outputTheSystemPropertiesObject() {
+        Properties properties = System.getProperties();
+        properties.list(System.out);
+    }
+
+    @Test
+    public void storeAndLoadASavedPropertiesFile() throws IOException {
+        Properties properties = new Properties();
+        properties.setProperty("key1", "value1");
+        properties.setProperty("key2", "value2");
+        String tempDirectory = System.getProperty("java.io.tmpdir");
+        String tempFilePath = tempDirectory + "tempFile.properties";
+        FileOutputStream stream = new FileOutputStream(tempFilePath);
+        properties.store(stream, "Elo");
+        stream.close();
+        FileReader reader = new FileReader(tempFilePath);
+        Properties loaded = new Properties();
+        try {
+            loaded.load(reader);
+        } finally {
+            reader.close();
+        }
+        assertEquals("value1", loaded.getProperty("key1"));
+        assertEquals("value2", loaded.getProperty("key2"));
+        new File(tempFilePath).delete();
+    }
+}
